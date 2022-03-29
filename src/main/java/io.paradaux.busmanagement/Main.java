bd8ef565c6e8b8f258f082cd2664f34ed7976728
Rían Errity <rian@paradaux.io>
2022-03-29 11:02:26 +0100

    Add main method to make it executable

--- src/main/java/io.paradaux.busmanagement/Main.java
+++ src/main/java/io.paradaux.busmanagement/Main.java
@@ -1,9 +1,5 @@
 package io.paradaux.busmanagement;
 
-import java.util.List;
-
-import static io.paradaux.busmanagement.parse.CSVParser.readFile;
-
 public class Main {
     public static void main(String[] args) {
         throw new UnsupportedOperationException("Not yet implemented.");
