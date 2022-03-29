377c56f674bba384a795334cf31c99ea6c949ee1
Rían Errity <rian@paradaux.io>
2022-03-29 10:59:44 +0100

    Gradle Build System

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,25 @@ group = "io.paradaux.busmanagement"
 description = "Bus Management for Toronto City Data"
 val projectVersion: String by project
 version = projectVersion
+
+plugins {
+    application
+}
+
+repositories {
+    gradlePluginPortal()
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    implementation("com.google.guava:guava:31.1-jre")
+}
+
+application {
+    mainClass.set("io.paradaux.busmanagement.Main")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
