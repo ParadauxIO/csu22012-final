4f480147b66c9987c3927558b0ab4b4f87578fc3
Rían Errity <rian@paradaux.io>
2022-04-04 12:03:50 +0100

    Add OpenJFX/JavaFX for GUI

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ version = projectVersion
 
 plugins {
     application
+    id("org.openjfx.javafxplugin") version "0.0.10"
     id("org.checkerframework") version "0.6.9"
 }
 
@@ -15,10 +16,20 @@ repositories {
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+
+    implementation("org.controlsfx:controlsfx:11.1.1")
+
+    implementation("com.dlsc.formsfx:formsfx-core:11.5.0")
+    implementation("org.kordamp.bootstrapfx:bootstrapfx-core:0.4.0")
 }
 
 application {
     mainClass.set("io.paradaux.busmanagement.Main")
+}
+
+javafx {
+    version = "11.0.2"
+    modules("javafx.controls", "javafx.fxml")
 }
 
 tasks.named<Test>("test") {
--- src/main/java/io/paradaux/busmanagement/Main.java
+++ src/main/java/io/paradaux/busmanagement/Main.java
@@ -1,12 +1,39 @@
 package io.paradaux.busmanagement;
 
 import io.paradaux.busmanagement.graph.BusNetwork;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
 
-public class Main {
+
+public class Main extends Application {
     public static void main(String[] args) {
         System.out.println("Bus Management System");
 
-        BusNetwork network = new BusNetwork();
+//        BusNetwork network = new BusNetwork();
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/main.fxml"));
+
+        Scene scene = null;
+        try {
+            scene = new Scene(loader.load(), 620, 400);
+        } catch (IOException e) {
+            System.err.println("An error occurred.");
+            System.exit(1);
+        }
+
+        stage.setTitle("Translink.ca | Journey Planner");
+
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
 
     }
+
 }
