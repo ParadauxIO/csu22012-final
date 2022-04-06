4c0a0706234c92e82f0126e60215ac96f7337afa
Rían Errity <rian@paradaux.io>
2022-04-06 16:53:43 +0100

    default controller class

--- src/main/java/io/paradaux/busmanagement/ui/controller/MainController.java
+++ src/main/java/io/paradaux/busmanagement/ui/controller/MainController.java
@@ -1,4 +1,34 @@
 package io.paradaux.busmanagement.ui.controller;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class MainController {
+
+    @FXML
+    // The reference of inputText will be injected by the FXML loader
+    private TextField inputText;
+
+    // The reference of outputText will be injected by the FXML loader
+    @FXML
+    private TextArea outputText;
+
+    // location and resources will be automatically injected by the FXML loader
+    @FXML
+    private URL location;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private void initialize() { }
+
+    @FXML
+    private void printOutput() {
+        outputText.setText(inputText.getText());
+    }
+
 }
