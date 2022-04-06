ca741aa5e9a71bd88b6d33dabe0c537302a96cac
Rían Errity <rian@paradaux.io>
2022-04-06 16:06:30 +0100

    Stop parsing between quotation marks and repl cleanup

--- src/main/java/io/paradaux/busmanagement/ui/BusManagementRepl.java
+++ src/main/java/io/paradaux/busmanagement/ui/BusManagementRepl.java
@@ -34,15 +34,25 @@ public class BusManagementRepl implements Runnable {
                 hasQuit = true;
             } else {
                 String[] command = operation.split(" ");
+                String[] parameters = removeFirstCountFromArray(1, command);
+                String conjoinedParameters = String.join(" ", parameters);
 
                 switch (command[0].toLowerCase(Locale.ROOT)) {
                     case "journey": {
-                        System.out.println("d");
+                        List<String> stops = parseSubstring('"', conjoinedParameters);
+
+                        if (stops.size() != 2) {
+                            System.out.println("Invalid options, please provide two stops surrounded by quotation marks.");
+                        }
+
+                        // TODO get the cost of going from stops[0] to stops[1]
                         break;
                     }
 
                     case "lookup": {
-                        System.out.println("l");
+
+
+
                         break;
                     }
 
@@ -65,7 +75,7 @@ public class BusManagementRepl implements Runnable {
     private void printHelp() {
         System.out.println("Translink Journey Planner | Repl Edition");
         System.out.println("Type '?' or 'help' at any time for further instructions.");
-        System.out.println("Strings containing spaces should be encapsulated with colons :LIKE SO:\n\n");
+        System.out.println("Strings containing spaces should be encapsulated with quotation marks \"Like this\"\n\n");
         System.out.println("COMMANDS ► ");
         printCommand("journey", "Plan a journey", "journey :STOP1: :STOP2:");
         printCommand("lookup", "Look up stop", "lookup <stop> {:stop:}\n");
@@ -75,5 +85,38 @@ public class BusManagementRepl implements Runnable {
 
     private void printCommand(String n, String d, String u) {
         System.out.println(String.format(" ► %s :: %s%n   %s", n, d, u));
+    }
+
+    private String[] removeFirstCountFromArray(int count, String[] arr) {
+        String[] arr2 = new String[arr.length-count];
+        System.arraycopy(arr, count, arr2, 0, arr2.length);
+        return arr2;
+    }
+
+    private List<String> parseSubstring(char delimiter, String s) {
+        System.out.println("s: " + s);
+        List<Integer> delimiterLocations = new ArrayList<>();
+
+        char[] letters = s.toCharArray();
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i] == delimiter) {
+                delimiterLocations.add(i);
+            }
+        }
+
+        System.out.println("delim size: " + delimiterLocations);
+
+        if (delimiterLocations.size() % 2 != 0) {
+            return new ArrayList<>();
+        }
+
+
+        List<String> stops = new ArrayList<>();
+        for (int i = 0; i < delimiterLocations.size() - 1; i+=2) {
+            stops.add(s.substring(delimiterLocations.get(i) + 1, delimiterLocations.get(i+1)));
+        }
+
+        System.out.println("stops" + stops);
+        return stops;
     }
 }
