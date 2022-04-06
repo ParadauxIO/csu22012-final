2231d305abadbf382ce5bdaa314e7a999d16ab57
Rían Errity <rian@paradaux.io>
2022-04-06 13:33:20 +0100

    Add parsed stops to the TST

--- src/main/java/io/paradaux/busmanagement/graph/BusNetwork.java
+++ src/main/java/io/paradaux/busmanagement/graph/BusNetwork.java
@@ -2,6 +2,7 @@ package io.paradaux.busmanagement.graph;
 
 import io.paradaux.busmanagement.data.Stop;
 import io.paradaux.busmanagement.data.parse.CSVParser;
+import io.paradaux.busmanagement.data.string.TernarySearchTree;
 
 import java.util.HashMap;
 import java.util.List;
@@ -11,6 +12,7 @@ public class BusNetwork {
 
     Map<Stop, List<Stop>> network;
     Map<Integer, Stop> stopRegistry;
+    TernarySearchTree stopNameTST;
 
     public BusNetwork() {
         network = new HashMap<>();
@@ -18,6 +20,7 @@ public class BusNetwork {
 
         // Registers the stop
         for (Stop s : CSVParser.parseStops()) {
+            stopNameTST.insert(s.getName());
             stopRegistry.put(s.getId(), s);
         }
 
