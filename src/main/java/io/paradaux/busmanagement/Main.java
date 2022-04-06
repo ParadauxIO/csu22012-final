c462a1745ce8817bbeecb70a4c78caaad6be79c2
Rían Errity <rian@paradaux.io>
2022-04-06 14:04:21 +0100

    Add option of using CLI, in the event I don't finish the GUI in time

--- src/main/java/io/paradaux/busmanagement/Main.java
+++ src/main/java/io/paradaux/busmanagement/Main.java
@@ -1,6 +1,8 @@
 package io.paradaux.busmanagement;
 
+import io.paradaux.busmanagement.graph.BusNetwork;
 import io.paradaux.busmanagement.ui.BusManagementGUI;
+import io.paradaux.busmanagement.ui.BusManagementRepl;
 import javafx.application.Application;
 
 
@@ -10,13 +12,25 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Bus Management System");
 
-//        BusNetwork network = new BusNetwork();
+        BusNetwork network = new BusNetwork();
 
         BusManagementGUI gui = new BusManagementGUI();
-        Application.launch(BusManagementGUI.class, args);
+
+        if (System.getenv("nogui").equals("true") || strArrayContains("-nogui", args)) {
+            BusManagementRepl repl = new BusManagementRepl(network);
+            repl.run();
+        } else {
+            Application.launch(BusManagementGUI.class, args);
+        }
     }
 
+    private static boolean strArrayContains(String str, String[] arr) {
+        for (String s : arr) {
+            if (str.equals(s)) {
+                return true;
+            }
+        }
 
-
-
+        return false;
+    }
 }
--- src/main/java/io/paradaux/busmanagement/ui/BusManagementRepl.java
+++ src/main/java/io/paradaux/busmanagement/ui/BusManagementRepl.java
@@ -1,4 +1,79 @@
 package io.paradaux.busmanagement.ui;
 
-public class BusManagementRepl {
+import io.paradaux.busmanagement.graph.BusNetwork;
+
+import java.util.*;
+
+public class BusManagementRepl implements Runnable {
+
+    private static final Set<String> EXIT_COMMANDS;
+
+    static {
+        final SortedSet<String> exitCommands = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
+        Collections.addAll(exitCommands, "exit", "quit", "leave");
+        EXIT_COMMANDS = Collections.unmodifiableSortedSet(exitCommands);
+    }
+
+    private final BusNetwork network;
+
+    public BusManagementRepl(BusNetwork network) {
+        this.network = network;
+    }
+
+    @Override
+    public void run() {
+        Scanner consoleIn = new Scanner(System.in);
+        printHelp();
+
+        boolean hasQuit = false;
+        do {
+            System.out.print("journey-planner $ ");
+            String operation = consoleIn.nextLine();
+
+            if (EXIT_COMMANDS.contains(operation)) {
+                hasQuit = true;
+            } else {
+                String[] command = operation.split(" ");
+
+                switch (command[0].toLowerCase(Locale.ROOT)) {
+                    case "journey": {
+                        System.out.println("d");
+                        break;
+                    }
+
+                    case "lookup": {
+                        System.out.println("l");
+                        break;
+                    }
+
+                    case "?":
+                    case "help": {
+                        printHelp();
+                        break;
+                    }
+
+                    default: {
+                        System.out.println("Invalid operation. Type '?' or 'help' for instructions.");
+                        break;
+                    }
+                }
+            }
+        } while(!hasQuit);
+
+    }
+
+    private void printHelp() {
+        System.out.println("Translink Journey Planner | Repl Edition");
+        System.out.println("Type '?' or 'help' at any time for further instructions.");
+        System.out.println("Strings containing spaces should be encapsulated with colons :LIKE SO:\n\n");
+        System.out.println("COMMANDS ► ");
+        printCommand("journey", "Plan a journey", "journey :STOP1: :STOP2:");
+        printCommand("lookup", "Look up stop", "lookup <stop> {:stop:}\n");
+        System.out.println("Parameters are delimited by space.");
+        System.out.println("(<> denotes a required parameter, [] denotes an optional parameter, {} denotes a list of possible parameters)");
+    }
+
+    private void printCommand(String n, String d, String u) {
+        System.out.println(String.format(" ► %s :: %s%n   %s", n, d, u));
+    }
 }
