0916f035671c10af82ec57ba8dc195f93809ca45
Rían Errity <rian@paradaux.io>
2022-04-09 19:59:33 +0100

    Simplified graph connection POJO impl

---
+++ src/main/java/io/paradaux/busmanagement/graph/StopNode.java
@@ -0,0 +1,20 @@
+package io.paradaux.busmanagement.graph;
+
+public class StopNode {
+
+    private final int stopId;
+    private final double cost;
+
+    public StopNode(int stopId, double cost) {
+        this.stopId = stopId;
+        this.cost = cost;
+    }
+
+    public int getStopId() {
+        return stopId;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
